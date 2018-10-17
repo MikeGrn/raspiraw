9b7ba19a0d2a050db3e62d7c10931636cb539931
Aleksey Zhidkov <aleksey.zhidkov@pm.me>
2018-10-17 19:38:24 +0700

    Начал приводить i2c_test к нормальному тулу для управления камерой

--- camcon.c
+++ camcon.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <string.h>
 
 #include <linux/i2c.h>
 #include <linux/i2c-dev.h>
@@ -26,7 +27,6 @@ void reg_to_buff(uint16_t reg, unsigned char *buf) {
 	buf[0] = (reg >> 8) & 0xFF;
 	buf[1] = (reg >> 0) & 0xFF;
 }
-
 
 int
 i2c_write16(int i2c_fd, int addr,uint16_t reg, uint16_t value) {
@@ -57,7 +57,7 @@ i2c_write16(int i2c_fd, int addr,uint16_t reg, uint16_t value) {
 // reg = 0x30b2; // temp value
 // reg = 0x30b4; // temp controller
 // reg = 0x301a; // stream
-void read_register(int fd, uint8_t i2c_addr, uint16_t reg) {
+ssize_t read_register(int fd, uint8_t i2c_addr, uint16_t reg, uint16_t *value) {
 
 		unsigned char out_buf[4];
 		uint8_t buf[2];
@@ -81,10 +81,36 @@ void read_register(int fd, uint8_t i2c_addr, uint16_t reg) {
 		msgset.msgs = msgs;
 		msgset.nmsgs = 2;
 		ssize_t r = ioctl(fd, I2C_RDWR, &msgset);
-		printf("%d: %02X%02X=%02X%02x\n", r, msgs[0].buf[0], msgs[0].buf[1], out_buf[0], out_buf[1]);
+		*value = out_buf[0] << 8 | out_buf[1];
+		return r;
 }
 
-void main() {
+void show_register(int fd, uint8_t i2c_addr, uint16_t reg) {
+	uint16_t value;
+	ssize_t r = read_register(fd, i2c_addr, reg, &value);
+	if (r) {
+		printf("%04X=%04X\n", reg, value);
+	} else {
+		printf("Failed\n");
+	}
+}
+
+void write_register(int fd, uint8_t i2c_addr, uint16_t reg, uint16_t value) {
+	unsigned char out_buf[4];
+	reg_to_buff(reg, out_buf);
+	reg_to_buff(value, &out_buf[2]);
+	ssize_t r = write(fd, out_buf, 4);
+	if (r != 4) {
+		printf("Failed\n");
+	}
+}
+
+int main(int argc, char **argv) {
+	if (argc == 1) {
+		printf("Options: show_streaming|start_streaming|stop_streaming\n");
+		return -1;
+	}
+
 	int file;
 	char *filename = "/dev/i2c-0";
 	if ((file = open(filename, O_RDWR)) < 0) {
@@ -97,6 +123,18 @@ void main() {
 	    printf("Failed to acquire bus access and/or talk to slave.\n");
 	    /* ERROR HANDLING; you can check errno to see what went wrong */
 	    goto close_file;
+	}
+
+	if (strcmp(argv[1], "show_streaming") == 0) {
+		show_register(file, 0x10, 0x301a);
+	} else if (strcmp(argv[1], "start_streaming") == 0) {
+		show_register(file, 0x10, 0x301a);
+		write_register(file, 0x10, 0x301a, 0x205C);
+		show_register(file, 0x10, 0x301a);
+	} else if (strcmp(argv[1], "stop_streaming") == 0) {
+		show_register(file, 0x10, 0x301a);
+		write_register(file, 0x10, 0x301a, 0x2058);
+		show_register(file, 0x10, 0x301a);
 	}
 
 int r;
@@ -117,7 +155,7 @@ int r;
 	//read_register(file, 0x10, 0x30b2);
 
 
-	printf("Streaming: ");
+/*	printf("Streaming: ");
 	read_register(file, 0x10, 0x301a);
 
 	unsigned char out_buf[4];
@@ -127,55 +165,8 @@ int r;
 	r = write(file, out_buf, 4);
 
 	printf("Streaming: ");
-	read_register(file, 0x10, 0x301a);
+	read_register(file, 0x10, 0x301a);*/
 
-/*	ssize_t r;
-	uint16_t i = 0x301a;
-	r = command(i, file);
-	uint16_t reg = i;
-	unsigned char out_buf[4];
-	reg_to_buff(reg, out_buf);
-	out_buf[3] = 0b01011100;
-	out_buf[2] = 0b100000;
-	r = write(file, out_buf, 4);
-	if (r != 4) {
-		perror("Error");
-		goto close_file;
-	}
-*/
-		/*uint8_t i2c_addr = 0x10;
-		//reg = 0x30B4;
-		// reg = 0x30b6; // temp calibration
-		// reg = 0x30b2; // temp value
-		// reg = 0x30b4; // temp controller
-		// reg = 0x301a; // stream
-		reg = 0x301a;
-		uint8_t buf[2];
-		reg_to_buff(reg, buf);
-		struct i2c_rdwr_ioctl_data msgset;
-		struct i2c_msg msgs[2] = {
-			{
-				 .addr = i2c_addr,
-				 .flags = 0,
-				 .len = 2,
-				 .buf = buf,
-			},
-			{
-				.addr = i2c_addr,
-				.flags = I2C_M_RD,
-				.len = 2,
-				.buf = out_buf,
-			},
-		};
-
-		msgset.msgs = msgs;
-		msgset.nmsgs = 2;
-		r = ioctl(file, I2C_RDWR, &msgset);
-		//printf("%d\n", r);
-		//printf("Read i2c addr %02X, reg %02X%02X (len %d), value %02X %02X, err %d\n", i2c_addr, msgs[0].buf[1], msgs[0].buf[0], msgs[0].len, out_buf[0], out_buf[1], r);
-		
-		printf("%02X%02X=%02X%02x\n", msgs[0].buf[0], msgs[0].buf[1], out_buf[0], out_buf[1]);
-		*/
 
 close_file:
 	close(file);
